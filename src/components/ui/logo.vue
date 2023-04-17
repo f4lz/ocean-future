ed37a5145457ea1f1be1349d8e70a913282e0d66
nzb3 <root123.ap@gmail.com>
2023-04-17 18:12:48 +0300

    component logo

---
+++ src/components/ui/logo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img    
+            src="@/assets/images/logo.svg" 
+            alt="logo dolphin and sea" 
+            :style="{ width: size + 'px', height: size + 'px' }"
+        >
+    </div>
+</template>
+  
+  <script lang="ts">
+  
+  import { defineComponent, defineProps } from 'vue'
+  
+  const props = defineProps( {
+    size: {
+        type: Number,
+        requaired: true,
+        default: () => 100,
+    },
+  } )
+
+  export default defineComponent( {
+    setup () {
+    }
+  } )
+  </script>
