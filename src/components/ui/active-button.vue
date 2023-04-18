9dfb78120308a061da5d2dcec9495f9eb029bc07
Илья Михайлов <ilia-mikhailov-03@mail.ru>
2023-04-18 21:55:39 +0300

    active button style

--- src/components/ui/active-button.vue
+++ src/components/ui/active-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-bind="$attrs">
+  <button v-bind="$attrs" class="button__feedback">
     <slot>
       Обратная связь
     </slot>
@@ -15,3 +15,22 @@ export default defineComponent( {
 } )
 
 </script>
+
+<style lang="scss">
+  .button__feedback{
+    display: flex;
+    padding: 10px 18px;
+    background-color: #181A1B;
+    color: white;
+    border: none;
+    
+    &:hover{
+      background-color: #1e2224;
+      transition: 0.7s;
+    }
+
+    &:active{
+      background-color: #4b4c4d;
+    }
+  } 
+</style>
